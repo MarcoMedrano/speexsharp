8dc578b3f2e83bac99a923cdba5edb423de3c5c2
Marco Medrano <Marco.Medrano.Garay@gmail.com>
2015-12-02 10:56:49 -0400

    Hola willy :S haha

--- speexcmd/Program.cs
+++ speexcmd/Program.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace speexcmd
 {
-    //testing credentials
+    //testing credentials WORKING branch
     class Program
     {
         static void Main(string[] args)
