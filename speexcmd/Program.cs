872fff4fe0f08f9bc5b42b44382b87d9885af592
Willy Soria Guzmán <willy.soriaguzman@outlook.com>
2015-12-02 10:56:27 -0400

    Testing credentials working branch

--- speexcmd/Program.cs
+++ speexcmd/Program.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace speexcmd
 {
-    //testing credentials
+    //testing credentials working branch
     class Program
     {
         static void Main(string[] args)
