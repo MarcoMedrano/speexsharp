4f387ec0554ba691b2074f8377da389c443dfe53
Marco Medrano <Marco.Medrano.Garay@gmail.com>
2015-12-04 16:04:08 -0400

    Added project ot test nspeex

---
+++ nspeexcmd/Program.cs
@@ -0,0 +1,29 @@
+﻿using NSpeex;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nspeexcmd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Decode();
+        }
+
+        private static void Decode()
+        {
+            byte[] encodedData = File.ReadAllBytes("speex.sample1.encoded.spx");
+            SpeexDecoder decoder = new SpeexDecoder(BandMode.Wide);
+
+            short[] decodedFrame = new short[1024]; // should be the same number of samples as on the capturing side
+            decoder.Decode(encodedData, 0, encodedData.Length, decodedFrame, 0, false);
+
+            // todo: do something with the decoded data
+        }
+    }
+}
